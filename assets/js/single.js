8b32ab89c699cf8c0e72b18f41ecf371d3da8173
tgtiburon <tg.tiburon@gmail.com>
2021-11-03 10:27:00 -0400

    finished list-issues

---
+++ assets/js/single.js
@@ -0,0 +1,95 @@
+console.log("single.js is loaded");
+
+let issueContainerEl = document.querySelector("#issues-container");
+let limitWarningEl = document.querySelector("#limit-warning");
+
+
+
+
+const getRepoIssues = (repo) => {
+    console.log("repo");
+    let apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
+    fetch(apiUrl).then(function(response) {
+        //request was successful
+        if(response.ok) {
+            response.json().then(function(data) {
+                // pass the response data to dom function
+                displayIssues(data);
+
+                // check if api has paginated issues
+                if (response.headers.get("Link")) {
+                    console.log("repo has more than 30 issues");
+                    displayWarning(repo);
+                }
+            });
+        } else {
+            alert("There was a problem with your request!");
+        }
+    });
+
+}
+
+const displayIssues = (issues) => {
+
+    if(issues.length === 0) {
+        issueContainerEl.textContent = "This repo has no open issues!";
+        return;
+    }
+    for (let i = 0; i < issues.length; i++) {
+        //const element = issues[i];
+        // create a link element to take users to the issue on github
+        let issueEl = document.createElement("a");
+        issueEl.classList = "list-item flex-row justify-space-between align-center";
+        issueEl.setAttribute("href", issues[i].html_url);
+        issueEl.setAttribute("target", "_blank");
+
+        //create span to hold issue title
+        let titleEl = document.createElement("span");
+        titleEl.textContent = issues[i].title;
+
+        // append to container
+        issueEl.appendChild(titleEl);
+
+        // create type element
+        let typeEl = document.createElement("span");
+
+        // check if issue is an actual issue or a pull request
+        if(issues[i].pull_request) {
+            typeEl.textContent = ("Pull request");
+        } else {
+            typeEl.textContent = ("Issue");
+        }
+
+        // append to container
+        issueEl.appendChild(typeEl);
+
+
+        issueContainerEl.appendChild(issueEl);
+
+
+        
+    }
+   
+
+}
+
+const displayWarning = (repo) => {
+    // add text to the warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    let linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+};
+
+
+
+
+
+
+
+getRepoIssues("facebook/react");
